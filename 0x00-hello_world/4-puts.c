141bb28a0df98b07d31e1721062682d2d106d650
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-07 08:56:06 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-char *message="\"Programming is like building a multilingual puzzle";    
+char *message= "\"Programming is like building a multilingual puzzle";
 /* Using puts to print the string */
 puts(message);
 return (0);
