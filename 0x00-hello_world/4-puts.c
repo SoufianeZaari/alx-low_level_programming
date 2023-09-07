9c79652b6abe3678bb84ff5f67a404eda379b251
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-07 09:05:17 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,12 @@ int main(void)
 {
 char *message = "\"Programming is like building a multilingual puzzle";
 /**
- * Using puts to print the string
- * main - Entry point
-*/
+ * @brief The main function of the program.
+ *
+ * This function is the entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
 puts(message);
 return (0);
 }
