868a0a2a350ceaba64ade23d36b82d84892cea5d
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-07 07:51:13 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int main() {
-	
-    const char *message = "\"Programming is like building a multilingual puzzle";
-    
-    /*  Using puts to print the string */
-    puts(message);
-
+int main(void)
+{
+char  *message = "\"Programming is like building a multilingual puzzle";    
+/*  Using puts to print the string */
+puts(message);
     return 0;
 }
