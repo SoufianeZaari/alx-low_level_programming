40f31860b947c6191f0b1ccfbbc26c5936967445
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-08 17:12:01 -0700

    C

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	if (n>0)
+		printf("%d is positive",&d);
+	if else (n=0)
+		printf("%d is zero",&d);
+	else
+		printf("%d is negative",&d);
+	return (0);
+}
