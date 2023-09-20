5794d5389c933a993f70f55860dc2b4ef0adee45
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-20 09:55:30 -0700

    for

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - Prints numbers between 0 to 9 and letters between a to f.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+int main()
 {
 int i;
-for (i = 48; i < 58; i++)
+char first ='a', last= 'f' ,n;
+for(i=0; i<10; i++)
 {
-putchar(i);
+putchar('0' + i);
 }
-for (i = 97; i < 103; i++)
+for(n=first; n<=last; n++)
 {
-putchar(i);
+putchar(n);
 }
 putchar('\n');
-return (0);
+return 0;
 }
