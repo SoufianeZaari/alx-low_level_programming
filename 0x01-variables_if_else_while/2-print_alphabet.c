01ef9a70101a0977b81fb22acd94c0ed973e8b74
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-20 08:14:21 -0700

    for

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main()
-{
-    char first='a',last='z',i;
-    for (i=first; i<=last; i++)
-    {
-        putchar(i); // Display the current character
 
+int main() {
+    char first = 'a', last = 'z', i;
+    for (i = first; i <= last; i++) {
+        putchar(i); /* Display the current character */
     }
-     putchar('\n');
+    putchar('\n');
     return 0;
 }
-
