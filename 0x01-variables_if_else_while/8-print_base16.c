3963097cd8509f4edac57254799b4ee48072d5e0
Soufiane <zari.soufiane@etu.uae.ac.ma>
2023-09-20 09:58:03 -0700

    for

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
 int i;
-char first ='a', last= 'f' ,n;
-for(i=0; i<10; i++)
+char first = 'a' , last = 'f' , n;
+for(i = 0; i < 10; i++)
 {
 putchar('0' + i);
 }
-for(n=first; n<=last; n++)
+for(n = first; n <= last; n++)
 {
 putchar(n);
 }
 putchar('\n');
-return 0;
+return (0);
 }
