21f4c8073de2bf8f80fb6778f9914ce30416e939
SoufianeZaari <zari.soufiane@etu.uae.ac.ma>
2023-10-19 12:45:40 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
 size_t s = 0;
 while (h)
 {
-if 
-(!h->str) printf("[0] (nil)\n");
+if (!h->str) 
+printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
